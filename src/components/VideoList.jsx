7bf936c2056d481790e2a1be90d8580f5dafc63d
Sijia Tao <taoshika127@gmail.com>
2022-07-12 21:00:00 -0700

    Complete creating a dynamic Video List component

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,12 +1,15 @@
-var VideoList = () => (
-  <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-  </div>
-);
+//import exampleVideoData from '/src/data/exampleVideoData.js';
+import VideoListEntry from './VideoListEntry.js';
+
+var VideoList = (props) => {
+  console.log(props.videos);
+  return (<div className="video-list">
+    {props.videos.map(video => (
+      <VideoListEntry video={video} />
+    )
+    )}
+  </div>);
+};
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
