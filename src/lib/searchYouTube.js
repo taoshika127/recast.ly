b23fbb1e35bbf5cdbcad6cee25f95acc98fb4e02
Sijia Tao <taoshika127@gmail.com>
2022-07-16 13:25:55 -0700

    Advanced - approaching fetch api

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -17,6 +17,15 @@ var searchYouTube = (query, callback) => {
       console.error('chatterbox: Failed to fetch messages', error);
     }
   });
+  // var data = {'q': query};
+  // var url = new URL('https://app-hrsei-api.herokuapp.com/api/recastly/videos');
+  // for (let k in data) { url.searchParams.append(data[k]); }
+  // fetch(url, {
+  //   method: 'GET',
+  //   headers: {'Content-type': 'application/json;charset=UTF-8"'}
+  // })//api for the get request
+  //   .then(response => response.json())
+  //   .then(data => console.log(data));
 };
 
 export default searchYouTube;
